3579987c214b743fe7bee33b56366e1028407375
Dhafiyamayo <141246330+Dhafiyamayo@users.noreply.github.com>
2023-08-20 22:57:21 +0800

    start js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//tutorial js
+/* //printing the word
+console.log("I like pizza and ayam!");
+console.log("saya suka makan");
+
+window.alert("PERINGATAN WEB ANDA DIBLOKIR PEMERINTAH") */
+
+/*
+    bisa tidak ya saya istiqomah huhuhu. apakah belajar coding dan jadi web programmer adalah jalan yang tepat untukku? :"""??"
+*/
+
+let age;
+age = 22;
+
+console.log(age); 
+
