df4f62d1b0ea61a68880f671029fdb1662355705
Dhafiyamayo <141246330+Dhafiyamayo@users.noreply.github.com>
2023-08-24 23:13:49 +0800

    js update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.alert("PERINGATAN WEB ANDA DIBLOKIR PEMERINTAH") */
     bisa tidak ya saya istiqomah huhuhu. apakah belajar coding dan jadi web programmer adalah jalan yang tepat untukku? :"""??"
 */
 
-let firstName = "Bintang" //strings
+/*let firstName = "Bintang" //strings
 let age = 22; //numbers
 let student = false; //booleans
 
@@ -18,18 +18,60 @@ console. log ("Hello", firstName);
 console.log("you are", age, "right?"); 
 console.log ("are you a student of Mulawarman University?:", student);
 
-document.getElementById("tes1").innerHTML = "Hello" + firstName;
-document.getElementById("tes2").innerHTML = "you are" + age + " right?";
-document.getElementById("tes3").innerHTML = "are you a student of Mulawarman University?:" + student
+document.getElementById("tes1").innerHTML = "Hello " + firstName;
+document.getElementById("tes2").innerHTML = "you are " + age + " years old right?";
+document.getElementById("tes3").innerHTML = "are you a student of Mulawarman University? : " + student
 
-let username1 = window.prompt("Isi nama data diri kamu di sini");
-console.log(username1);
+/* let firstusername = window.prompt("Isi nama data diri kamu di sini");
+console.log(firstusername); */
 
-let username;
+letlet age = window.prompt("Berapa umur kamu?")
+
+console.log(typeof age)
+ageage = Number(aletalet
+//age += 1
+
+console.log("Wah kamu sekarang sudah", age, "tahun")
+aletale
+/* et username;
 
 document.getElementById("tombolsubmit").oneclick = function(){
 
     username = document.getElementById("mytext").value;
     console.log(username);
     document.getElementById("namapanggilan").innerHTML = "hello" + username;
+} */
+
+
+// Type conversion = change the datatype of a value to another (strings, number, booleans)
+/*
+let x;
+let y;
+let z;
+
+x = Number ("3.14")
+y = Boolean (3.14)
+z = String (3.14)
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
+*/
+
+let a
+let b
+let c
+
+document.getElementById("CalculateButton").oneclick = function(){
+
+    a = document.getElementById("textA").value
+    a = Number(a);
+
+    b = document.getElementById("textB").value
+    b = Number(b);
+
+    c = Math.sqrt(Math.pow(a,2) + Math.pow(b, 2))
+
+    document.getElementById("sideC").innerHTML = "side C:" + c
+
 }
